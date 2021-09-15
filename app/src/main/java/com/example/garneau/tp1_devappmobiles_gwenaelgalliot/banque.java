477e917cbf71937c87eb74f62ec8da38b00d2b87
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2021-09-14 23:06:46 -0400

    creation base du code de chaque page

---
+++ app/src/main/java/com/example/garneau/tp1_devappmobiles_gwenaelgalliot/banque.java
@@ -0,0 +1,18 @@
+package com.example.garneau.tp1_devappmobiles_gwenaelgalliot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class banque extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
